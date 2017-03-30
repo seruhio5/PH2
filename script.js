cc44e701f3463c188d9893276016b99c05e88ead
Unknown <sergiomolero5@gmail.com>
2017-03-30 12:23:57 +0200

    Entradas enlazadas e iconos puestos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function mostrarentradas(frm){
+function mostrarentradas(){
 	let xhr = new XMLHttpRequest(),
 		url = 'http://localhost/PH2/rest/entrada/';
 
@@ -16,16 +16,16 @@ function mostrarentradas(frm){
 				let e = v.FILAS[i],
 					foto = 'http://localhost/PH2/fotos/' + e.fichero;
 				html += '<article>'
-				html +=	'<h3>'+ e.nombre + '</h3>'
+				html +=	'<h3><a href="entrada.html?entrada=' +e.id+ '">'+ e.nombre + '</a></h3>'
 				html +=	'<div>'
 				html +=		'<img src="' + foto + '" alt="' + e.descripcion + '">'
 				html +=		'<p>' + e.descripcion + '</p>'
 				html +=	'</div>'
 				html +=	'<footer>'
-				html +=		'<p>'+ e.login +'</p>'
-				html +=		'<time>' + e.fecha + '</time>'
-				html +=		'<p>' + e.ncomentarios + '</p>'
-				html +=		'<p> '+ e.nfotos+' </p>'
+				html +=		'<p><i class="demo-icon icon-user"></i> '+ e.login +'</p>'
+				html +=		'<time><i class="demo-icon icon-calendar"></i> ' + e.fecha + '</time>'
+				html +=		'<p><i class="demo-icon icon-comment-empty"></i> Numero de comentarios: ' + e.ncomentarios + '</p>'
+				html +=		'<p><i class="demo-icon icon-picture"></i> Numero de fotos: '+ e.nfotos+' </p>'
 				html +=	'</footer>'
 				html +='</article>'
 			} //End for
@@ -34,4 +34,54 @@ function mostrarentradas(frm){
 	}
 	xhr.send();
 	return false;
+}
+function mostrarentrada(){
+	var id = getParameterByName('entrada');
+	console.log(id);
+	let xhr = new XMLHttpRequest(),
+		url = 'http://localhost/PH2/rest/entrada/';
+
+	xhr.open('GET', url, true);
+	//Cuando es get no se pasa nada por parametros, se concatena con la url
+	//url += '?pag=' + frm.pag.value + '&lpag=' + frm.lpag.value;
+	xhr.onload = function(){
+		console.log(xhr.responseText);
+		let v = JSON.parse(xhr.responseText);
+		console.log(v);
+
+		if(v.RESULTADO == 'ok'){
+			let html= '';
+			for(let i=0; i<v.FILAS.length; i++){
+				
+					let e = v.FILAS[i];
+				if(e.id==id){ //buscamos la id que nos piden
+					let foto = 'http://localhost/PH2/fotos/' + e.fichero;
+					html += '<article>'
+					html +=	'<h3>'+ e.nombre + '</h3>'
+					html +=		'<p><i class="demo-icon icon-user"></i> '+ e.login +'</p>'
+					html +=		'<time><i class="demo-icon icon-calendar"></i> ' + e.fecha + '</time>'
+					//html +=	'<div>'
+					//html +=		'<img src="' + foto + '" alt="' + e.descripcion + '">'
+					html +=	'<p>' + e.descripcion + '</p>'
+					//html +=	'</div>'
+					//html +=	'<footer>'
+					
+					
+					html +=		'<p><i class="demo-icon icon-picture"></i> Numero de comentarios: <a href="#comentarios">' + e.ncomentarios + '</a></p>'
+					html +=		'<p><i class="demo-icon icon-comment-empty"></i> Numero de fotos: <a href="#fotos">'+ e.nfotos+' </a></p>'
+					//html +=	'</footer>'
+					html +='</article>'
+				}
+			} //End for
+			document.getElementById('entrada').innerHTML = html;
+		}//end if
+	}
+	xhr.send();
+	return false;
+}
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
