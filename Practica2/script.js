96a9c168d4bac9c9db8778e763616b4d43914509
Unknown <sergiomolero5@gmail.com>
2017-04-04 20:04:13 +0200

    Index y entradas ya van bien

--- Practica2/script.js
+++ Practica2/script.js
@@ -86,7 +86,6 @@ function getParameterByName(name) {
 
 
 function mostrarcomentarios(){
-			console.log("HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 	let xhr = new XMLHttpRequest(),
 		url = 'http://localhost/PH2/Practica2/rest/comentario/?u=10';
 
@@ -99,11 +98,13 @@ function mostrarcomentarios(){
 		console.log(v);
 
 		if(v.RESULTADO == 'ok'){
-			console.log("HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 			let html= '';
+			html += '<h2>Ultimos 10 comentarios</h2>'
 			for(let i=0; i<v.FILAS.length; i++){
 				let e = v.FILAS[i];
+				
 				html += '<article>'
+
 				html +=	'<h3><i class="demo-icon icon-user"></i> '+ e.login +' </h3>'
 				html +=		'<time><i class="demo-icon icon-calendar"></i> ' + e.fecha + '</time>'
 				//html +=	'<div>'
@@ -113,9 +114,53 @@ function mostrarcomentarios(){
 				//html +=	'<footer>'
 				//html +=		'<a href="entrada.html?entrada=' +e.id_entrada+ '"><p><i class="demo-icon icon-doc-text"></i> ' + e.titulo + '</p></a>'
 				html +=		'<a href="entrada.html?entrada=' +e.id_entrada+ '"><p>Titulo Comentario: ' + e.titulo + '</p></a>'
-				//html +=		'<p>'+ e.nfotos+' </p>'
+				html +=		'<p>'+ e.texto+' </p>'
 				//html +=	'</footer>'
 				html +='</article>'
+
+			} //End for
+			document.getElementById('comentarios').innerHTML = html;
+		}//end if
+	}
+	xhr.send();
+	return false;
+}
+
+function mostrarcomentariosid(){
+	var id = getParameterByName('entrada');
+	let xhr = new XMLHttpRequest(),
+		url = 'http://localhost/PH2/Practica2/rest/comentario/?id_entrada='+id;
+
+	xhr.open('GET', url, true);
+	//Cuando es get no se pasa nada por parametros, se concatena con la url
+	//url += '?pag=' + frm.pag.value + '&lpag=' + frm.lpag.value;
+	xhr.onload = function(){
+		console.log(xhr.responseText);
+		let v = JSON.parse(xhr.responseText);
+		console.log(v);
+
+		if(v.RESULTADO == 'ok'){
+			let html= '';
+			html += '<h2>Seccion de comentarios <i class="demo-icon icon-comment-empty"></i></h2>'
+			for(let i=0; i<v.FILAS.length; i++){
+				let e = v.FILAS[i];
+
+				html += '<article>'
+				
+				html +=	'<h3><i class="demo-icon icon-user"></i> '+ e.login +' </h3>'
+				html +=		'<time><i class="demo-icon icon-calendar"></i> ' + e.fecha + '</time>'
+				//html +=	'<div>'
+				//html +=		'<img src="' + foto + '" alt="' + e.descripcion + '">'
+				//html +=		'<p>' + e.descripcion + '</p>'
+				//html +=	'</div>'
+				//html +=	'<footer>'
+				//html +=		'<a href="entrada.html?entrada=' +e.id_entrada+ '"><p><i class="demo-icon icon-doc-text"></i> ' + e.titulo + '</p></a>'
+				html +=		'<p>Titulo Comentario: ' + e.titulo + '</p>'
+				html +=		'<p>'+ e.texto+' </p>'
+				html +=     '<a href="#form" class="contestar">Contestar</a>'
+				//html +=	'</footer>'
+				html +='</article>'
+
 
 			} //End for
 			document.getElementById('comentarios').innerHTML = html;
