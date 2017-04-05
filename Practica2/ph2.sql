7d772d53acb10f28ab8a275912e9655792d5592d
Unknown <sergiomolero5@gmail.com>
2017-04-05 18:58:47 +0200

    BASE DE DATOS

--- Practica2/ph2.sql
+++ Practica2/ph2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2017 at 10:27 AM
+-- Generation Time: Apr 05, 2017 at 06:58 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
