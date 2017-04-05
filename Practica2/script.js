5bc73b1d30466c2c001ab64e639864672af04950
Unknown <sergiomolero5@gmail.com>
2017-04-05 19:13:36 +0200

    Prueba

--- Practica2/script.js
+++ Practica2/script.js
@@ -169,3 +169,48 @@ function mostrarcomentariosid(){
 	xhr.send();
 	return false;
 }
+
+function mostrar_fotos_entrada(){
+	var id = getParameterByName('entrada');
+	console.log(id);
+	let xhr = new XMLHttpRequest(),
+		url = 'http://localhost/PH2/Practica2/rest/entrada/';
+
+	xhr.open('GET', url, true);
+	//Cuando es get no se pasa nada por parametros, se concatena con la url
+	//url += '?pag=' + frm.pag.value + '&lpag=' + frm.lpag.value;
+	xhr.onload = function(){
+		console.log(xhr.responseText);
+		let v = JSON.parse(xhr.responseText);
+		console.log(v);
+
+		if(v.RESULTADO == 'ok'){
+
+			let []foto = 'http://localhost/PH2/Practica2/rest/entrada/'+id+'/fotos';
+			console.log(foto[1]);
+			let html= '';
+			/*for(let i=0; i<v.FILAS.length; i++){
+				
+					let e = v.FILAS[i];
+					html += '<article>'
+					html +=	'<h3>'+ e.nombre + '</h3>'
+					html +=		'<p><i class="demo-icon icon-user"></i> '+ e.login +'</p>'
+					html +=		'<time><i class="demo-icon icon-calendar"></i> ' + e.fecha + '</time>'
+					//html +=	'<div>'
+					//html +=		'<img src="' + foto + '" alt="' + e.descripcion + '">'
+					html +=	'<p>' + e.descripcion + '</p>'
+					//html +=	'</div>'
+					//html +=	'<footer>'
+					
+					
+					html +=		'<p><i class="demo-icon icon-picture"></i> Numero de comentarios: <a href="#comentarios">' + e.ncomentarios + '</a></p>'
+					html +=		'<p><i class="demo-icon icon-comment-empty"></i> Numero de fotos: <a href="#fotos">'+ e.nfotos+' </a></p>'
+					//html +=	'</footer>'
+					html +='</article>'
+			} //End for*/
+			document.getElementById('fotos').innerHTML = html;
+		}//end if
+	}
+	xhr.send();
+	return false;
+}
