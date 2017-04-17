7a9af3fd6654123d4aa17d2857ebad3bf7701ffa
Unknown <sergiomolero5@gmail.com>
2017-04-17 20:15:46 +0200

    Menuses y logines

--- Practica2/script.js
+++ Practica2/script.js
@@ -207,33 +207,439 @@ function mostrar_fotos_entrada(){
 }
 function hacerlogin(frm){
     
-	if (sessionStorage.getItem('algo') == null){
-    }
+	
 	let xhr = new XMLHttpRequest(),
 		url = 'http://localhost/PH2/Practica2/rest/login/',	//Puesto para mi ruta
 		fd  = new FormData(frm);	//Mete todos los valores del formulario automaticamente
 
 	xhr.open('POST', url, true);
 	xhr.onload = function(){	//Cuando llega al paso 4 realiza la ejecudion de este codigo
-		console.log(xhr.responseText);	//Muestra la respuesta del proceso por consola
 		let du = JSON.parse(xhr.responseText);	
 		//Lo que hace es guardarlo en el sesion storage si ha funcionado
 		if(du.RESULTADO == 'ok'){
 			//console.log(du);
-			sessionStorage['algo'] = "algo"; //se podria utilizar la funcion stringiflay!
+			sessionStorage['error'] = undefined;
+			sessionStorage['clave'] = du.clave; //se podria utilizar la funcion stringiflay!
+			sessionStorage['login'] = du.login;
+			sessionStorage['nombre'] = du.nombre;
+			sessionStorage['email'] = du.email;
+			sessionStorage['ultimo_acceso'] = du.ultimo_acceso;
+			mostrarMensajeLogin();
 		}
-		
 		else{
-
+			sessionStorage['error'] = "El usuario y/o contraseña son incorrectos";
+			mostrarMensajeLogin();
 		}
 	};
 	xhr.send(fd);
 	return false;
 }
 function comprobar(){
-	/*console.log("añsldkfj");
-	console.log(sessionStorage['usu']);
-	if(sessionStorage.getItem('usu')!=null){
-		//window.location="http://localhost/PH2/Practica2/index.html";
-	}*/
+	if(sessionStorage.getItem('login')!=null){
+		window.location="http://localhost/PH2/Practica2/index.html";
+	}
+}
+function mostrarMensajeLogin(){
+	let capa_fondo=document.createElement('div'),capa_frente=document.createElement('article');
+	capa_fondo.appendChild(capa_frente);
+	let html="";
+	
+	if(sessionStorage['nombre']!=null){
+	html +='<h2>ACIERTO</h2>';
+	html +='<p>Bienvenido '+sessionStorage['nombre']+'</p>';
+	html +='<p>Ultima visita: '+sessionStorage['ultimo_acceso']+'</p>';
+	}else{
+		html +='<h2>ERROR</h2>';
+		html +='<p>'+sessionStorage['error']+'</p>';
+	}
+	html += '<button onclick="this.parentNode.parentNode.remove();comprobar();">Cerrar</button>';
+
+	capa_frente.innerHTML=html;
+	capa_fondo.classList.add('capa-fondo');
+	capa_frente.classList.add('capa-frente');
+
+	document.body.appendChild(capa_fondo);
+}
+function comprobar2(){
+	if(sessionStorage.getItem('login')==null){
+		window.location="http://localhost/PH2/Practica2/index.html";
+	}
+}
+function menuindex(){
+	let html="";
+	if(sessionStorage['nombre']!=null){
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu" id="active">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="#">';
+	html +='<i class="demo-icon icon-logout-1"></i>Logout';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="nueva-entrada.html">';
+	html +='<i class="demo-icon icon-doc"></i>Nueva entrada';
+	html +='</a>';
+	html +='</li>';
+	}
+	else{
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu" id="active">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="login.html">';
+	html +='<i class="demo-icon icon-login"></i>Login';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="registro.html">';
+	html +='<i class="demo-icon icon-user-plus"></i>Registro';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	}
+	document.getElementById('menu').innerHTML = html;					
+}
+function menulogin(){
+	let html="";
+	if(sessionStorage['nombre']!=null){
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="#">';
+	html +='<i class="demo-icon icon-logout-1"></i>Logout';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="nueva-entrada.html">';
+	html +='<i class="demo-icon icon-doc"></i>Nueva entrada';
+	html +='</a>';
+	html +='</li>';
+	}
+	else{
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu" id="active">';
+	html +='<a href="login.html">';
+	html +='<i class="demo-icon icon-login"></i>Login';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="registro.html">';
+	html +='<i class="demo-icon icon-user-plus"></i>Registro';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	}
+	document.getElementById('menu').innerHTML = html;					
+}
+function menuregistro(){
+	let html="";
+	if(sessionStorage['nombre']!=null){
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="#">';
+	html +='<i class="demo-icon icon-logout-1"></i>Logout';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="nueva-entrada.html">';
+	html +='<i class="demo-icon icon-doc"></i>Nueva entrada';
+	html +='</a>';
+	html +='</li>';
+	}
+	else{
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="login.html">';
+	html +='<i class="demo-icon icon-login"></i>Login';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu" id="active">';
+	html +='<a href="registro.html">';
+	html +='<i class="demo-icon icon-user-plus"></i>Registro';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	}
+	document.getElementById('menu').innerHTML = html;					
+}
+function menubuscar(){
+	let html="";
+	if(sessionStorage['nombre']!=null){
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu" id="active">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="#">';
+	html +='<i class="demo-icon icon-logout-1"></i>Logout';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="nueva-entrada.html">';
+	html +='<i class="demo-icon icon-doc"></i>Nueva entrada';
+	html +='</a>';
+	html +='</li>';
+	}
+	else{
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="login.html">';
+	html +='<i class="demo-icon icon-login"></i>Login';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="registro.html">';
+	html +='<i class="demo-icon icon-user-plus"></i>Registro';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu" id="active">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	}
+	document.getElementById('menu').innerHTML = html;					
+}
+function menulogout(){
+	let html="";
+	if(sessionStorage['nombre']!=null){
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu" id="active">';
+	html +='<a href="#">';
+	html +='<i class="demo-icon icon-logout-1"></i>Logout';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="nueva-entrada.html">';
+	html +='<i class="demo-icon icon-doc"></i>Nueva entrada';
+	html +='</a>';
+	html +='</li>';
+	}
+	else{
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="login.html">';
+	html +='<i class="demo-icon icon-login"></i>Login';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="registro.html">';
+	html +='<i class="demo-icon icon-user-plus"></i>Registro';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	}
+	document.getElementById('menu').innerHTML = html;					
+}
+function menunuevaentrada(){
+	let html="";
+	if(sessionStorage['nombre']!=null){
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="#">';
+	html +='<i class="demo-icon icon-logout-1"></i>Logout';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu" id="active">';
+	html +='<a href="nueva-entrada.html">';
+	html +='<i class="demo-icon icon-doc"></i>Nueva entrada';
+	html +='</a>';
+	html +='</li>';
+	}
+	else{
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="login.html">';
+	html +='<i class="demo-icon icon-login"></i>Login';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="registro.html">';
+	html +='<i class="demo-icon icon-user-plus"></i>Registro';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	}
+	document.getElementById('menu').innerHTML = html;					
+}
+function menuentrada(){
+	let html="";
+	if(sessionStorage['nombre']!=null){
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="#">';
+	html +='<i class="demo-icon icon-logout-1"></i>Logout';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="nueva-entrada.html">';
+	html +='<i class="demo-icon icon-doc"></i>Nueva entrada';
+	html +='</a>';
+	html +='</li>';
+	}
+	else{
+	html +='<li>';
+	html +='<label for="ckb-menu">&equiv;</label>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="index.html">';
+	html +='<i class="demo-icon icon-home-1"></i>Inicio';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="login.html">';
+	html +='<i class="demo-icon icon-login"></i>Login';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="registro.html">';
+	html +='<i class="demo-icon icon-user-plus"></i>Registro';
+	html +='</a>';
+	html +='</li>';
+	html +='<li class="menu">';
+	html +='<a href="buscar.html">';
+	html +='<i class="demo-icon icon-search-1"></i>Buscar';
+	html +='</a>';
+	html +='</li>';
+	}
+	document.getElementById('menu').innerHTML = html;					
 }
